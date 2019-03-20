f01a20ff167a611c02968e6947d2727f7bd6c3b2
Adam Wadsworth <adamkwadsworth@icloud.com>
2019-03-20 10:38:56 +0000

    Vue Cli
    Laracast Vue Cli Tutorial 16

---
+++ webpack-and-vue-cli/src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="box">
+        <p><slot></slot></p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+.box {
+    background-color:gray;
+    padding:10px;
+    border:solid 1px black;
+}
+</style>
